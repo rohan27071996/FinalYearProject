aedfbfac3eceda0e787473a11322842ad0224031
shubham193 <46805609+shubham193@users.noreply.github.com>
2019-03-26 14:54:04 +0530

    Add files via upload

---
+++ script/registration.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+		{$("span").click(function() {
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "text") {
+    input.attr("type", "password");
+  } else {
+    input.attr("type", "text");
+  }
+})});
+
